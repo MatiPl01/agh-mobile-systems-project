670629375269d7550d5119da05199429260a12f1
dariusz.piwowarski <d.piwowarski.mail@gmail.com>
2025-11-18 03:21:58 +0100

    add navigation

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { StatusBar, useColorScheme } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import RootNavigator from '@/navigation/RootNavigator';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaProvider>
+      <ThemeProvider value={isDarkMode ? DarkTheme : DefaultTheme}>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <RootNavigator />
+      </ThemeProvider>
+    </SafeAreaProvider>
+  );
+}
+
+export default App;
