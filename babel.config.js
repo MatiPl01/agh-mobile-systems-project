3ef9399359133109b3775502f046dc3e7e904466
dariusz.piwowarski <d.piwowarski.mail@gmail.com>
2025-11-29 01:26:14 +0100

    working camera image recognition

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-    plugins: [
+  presets: ['module:@react-native/babel-preset', '@babel/preset-typescript'],
+  plugins: [
+    ['@babel/plugin-proposal-optional-chaining'],
+    ['@babel/plugin-proposal-nullish-coalescing-operator'],
+    [
+      'react-native-worklets-core/plugin',
+      {
+        globals: ['__detectObjects'],
+      },
+    ],
     [
       'module-resolver',
       {
--- src/screens/ScannerScreen.tsx
+++ src/screens/ScannerScreen.tsx
@@ -1,3 +1,4 @@
+import Scanner from '@/components/Scanner';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import React from 'react';
@@ -6,10 +7,11 @@ import { StyleSheet } from 'react-native';
 export default function ScannerScreen() {
   return (
     <ThemedView style={styles.container}>
-      <ThemedText type='title'>Scanner</ThemedText>
+      <ThemedText type="title">Scanner</ThemedText>
+      <Scanner />
     </ThemedView>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
