7332be63c5bc51487765e6ea32ab9e64f0394fcd
Dariusz Piwowarski <d.piwowarski.mail@gmail.com>
2026-01-26 18:46:14 +0100

    Force light mode (#33)

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,14 @@ import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { StatusBar } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { useUnistyles } from 'react-native-unistyles';
 
 function AppContent() {
-  const { theme } = useUnistyles();
-
-  // TODO: Re-enable dark mode support once tile colors are adjusted
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
         <TensorflowModelProvider>
           <ThemeProvider value={DefaultTheme}>
-            <StatusBar
-              barStyle='dark-content'
-              backgroundColor={theme.colors.background}
-            />
+            <StatusBar />
             <RootNavigator />
           </ThemeProvider>
         </TensorflowModelProvider>
